618001d6d9fae913316a3942e56e43bf733c04c4
Ruletka <maksim.manezkiy.evg.99@gmail.com>
2023-01-02 21:05:26 +0300

    Refactoring for `SL_Situate.hpp` module

--- __StandardLibrary/SL_Situate.hpp
+++ __StandardLibrary/SL_Situate.hpp
@@ -17,16 +17,40 @@
             ::size_t D_Code;
             std::vector<std::string> D_Details;
         public:
-            void E_SetHeader (const std::string S_NewHeader) { this->D_Header = S_NewHeader; }
-            void E_SetBody (const std::string S_NewBody) { this->D_Body = S_NewBody; }
-            void E_SetCode (const ::size_t S_NewCode) { this->D_Code = S_NewCode; }
-            void E_PushDetail (const std::string S_NewDetail) { this->D_Details.push_back(S_NewDetail); }
-            void E_ClearDetail () { this->D_Details.clear(); }
+            void E_SetHeader (const std::string S_NewHeader) {
+                this->D_Header = S_NewHeader;
+            }
 
-            std::string E_GetHeader () { return this->D_Header; }
-            std::string E_GetBody () { return this->D_Body; }
-            ::size_t E_GetCode () { return this->D_Code; }
-            std::vector<std::string> E_GetDetails () { return this->D_Details; }
+            void E_SetBody (const std::string S_NewBody) {
+                this->D_Body = S_NewBody;
+            }
+
+            void E_SetCode (const ::size_t S_NewCode) {
+                this->D_Code = S_NewCode;
+            }
+
+            void E_PushDetail (const std::string S_NewDetail) {
+                this->D_Details.push_back(S_NewDetail);
+            }
+            void E_ClearDetail () {
+                this->D_Details.clear();
+            }
+
+            std::string E_GetHeader () {
+                return this->D_Header;
+            }
+
+            std::string E_GetBody () {
+                return this->D_Body;
+            }
+
+            ::size_t E_GetCode () {
+                return this->D_Code;
+            }
+            
+            std::vector<std::string> E_GetDetails () {
+                return this->D_Details;
+            }
         };
     }
 
