5602bb3ed8007b42953334e4a683e80533f3984e
Ruletka <maksim.manezkiy.evg.99@gmail.com>
2023-01-04 22:40:47 +0300

    fix errors

--- __Kernel/__Drivers/K_D_NakeFS.hpp
+++ __Kernel/__Drivers/K_D_NakeFS.hpp
@@ -34,17 +34,17 @@
                     fclose(D_FileController);
                     
                     D_Situate.E_SetHeader("__Kernel::__Drivers::E_FileExit()");
-                    D_Situate.E_SetBody(__StandardLibrary::E_ColorizeFront("E_FileExist", 197, 57, 235) + " returned success code!");
+                    D_Situate.E_SetBody(__StandardLibrary::E_ColorizeFont("E_FileExist", 197, 57, 235) + " returned success code!");
                     D_Situate.E_SetCode(0);
-                    D_Situate.E_PushDetail(__StandardLibrary::E_ColorizeFront("@File", 57, 146, 235) + " with name " + __StandardLibrary::E_ColorizeFront(S_FileName, 255, 0, 0) + " exist!");
+                    D_Situate.E_PushDetail(__StandardLibrary::E_ColorizeFont("@File", 57, 146, 235) + " with name " + __StandardLibrary::E_ColorizeFont(S_FileName, 255, 0, 0) + " exist!");
 
                     return D_Situate;
                 }
                 else {
                     D_Situate.E_SetHeader("__Kernel::__Drivers::E_FileExit()");
-                    D_Situate.E_SetBody(__StandardLibrary::E_ColorizeFront("E_FileExist", 197, 57, 235) + " returned error code!");
+                    D_Situate.E_SetBody(__StandardLibrary::E_ColorizeFont("E_FileExist", 197, 57, 235) + " returned error code!");
                     D_Situate.E_SetCode(1);
-                    D_Situate.E_PushDetail(__StandardLibrary::E_ColorizeFront("@File", 57, 146, 235) + " with name " + __StandardLibrary::E_ColorizeFront(S_FileName, 255, 0, 0) + " doesn't exist!");
+                    D_Situate.E_PushDetail(__StandardLibrary::E_ColorizeFont("@File", 57, 146, 235) + " with name " + __StandardLibrary::E_ColorizeFont(S_FileName, 255, 0, 0) + " doesn't exist!");
 
                     return D_Situate;
                 }
@@ -57,25 +57,25 @@
 
                 if (stat(S_DirectoryName.c_str(), &D_DirectoryInformation) != 0) {
                     D_Situate.E_SetHeader("__Kernel::__Drivers::E_DirectoryExist()");
-                    D_Situate.E_SetBody(__StandardLibrary::E_ColorizeFront("E_DirectoryExist", 197, 57, 235) + " returned error code!");
+                    D_Situate.E_SetBody(__StandardLibrary::E_ColorizeFont("E_DirectoryExist", 197, 57, 235) + " returned error code!");
                     D_Situate.E_SetCode(1);
-                    D_Situate.E_PushDetail("Can't access to " + __StandardLibrary::E_ColorizeFront("@Directory", 57, 146, 235) + " with name " + __StandardLibrary::E_ColorizeFront(S_DirectoryName, 255, 0, 0));
+                    D_Situate.E_PushDetail("Can't access to " + __StandardLibrary::E_ColorizeFont("@Directory", 57, 146, 235) + " with name " + __StandardLibrary::E_ColorizeFont(S_DirectoryName, 255, 0, 0));
 
                     return D_Situate;
                 }
                 else if (D_DirectoryInformation.st_mode& S_IFDIR) {
                     D_Situate.E_SetHeader("__Kernel::__Drivers::E_DirectoryExist()");
-                    D_Situate.E_SetBody(__StandardLibrary::E_ColorizeFront("E_DirectoryExist", 197, 57, 235) + " returned success code!");
+                    D_Situate.E_SetBody(__StandardLibrary::E_ColorizeFont("E_DirectoryExist", 197, 57, 235) + " returned success code!");
                     D_Situate.E_SetCode(0);
-                    D_Situate.E_PushDetail(__StandardLibrary::E_ColorizeFront("@Directory", 57, 146, 235) + " with name " + __StandardLibrary::E_ColorizeFront(S_DirectoryName, 255, 0, 0) + " exist!");
+                    D_Situate.E_PushDetail(__StandardLibrary::E_ColorizeFont("@Directory", 57, 146, 235) + " with name " + __StandardLibrary::E_ColorizeFont(S_DirectoryName, 255, 0, 0) + " exist!");
 
                     return D_Situate;
                 }
                 else {
                     D_Situate.E_SetHeader("__Kernel::__Drivers::E_DirectoryExist()");
-                    D_Situate.E_SetBody(__StandardLibrary::E_ColorizeFront("E_DirectoryExist", 197, 57, 235) + " returned error code!");
+                    D_Situate.E_SetBody(__StandardLibrary::E_ColorizeFont("E_DirectoryExist", 197, 57, 235) + " returned error code!");
                     D_Situate.E_SetCode(2);
-                    D_Situate.E_PushDetail(__StandardLibrary::E_ColorizeFront("@Directory", 57, 146, 235) + " with name " + __StandardLibrary::E_ColorizeFront(S_DirectoryName, 255, 0, 0) + " doesn't exist!");
+                    D_Situate.E_PushDetail(__StandardLibrary::E_ColorizeFont("@Directory", 57, 146, 235) + " with name " + __StandardLibrary::E_ColorizeFont(S_DirectoryName, 255, 0, 0) + " doesn't exist!");
 
                     return D_Situate;
                 }
@@ -86,9 +86,9 @@
 
                 if (E_FileExist(S_FileName).E_GetCode() == 0) {
                     D_Situate.E_SetHeader("__Kernel::__Drivers::E_CreateFile()");
-                    D_Situate.E_SetBody(__StandardLibrary::E_ColorizeFront("E_CreateFile", 197, 57, 235) + " returned error code!");
+                    D_Situate.E_SetBody(__StandardLibrary::E_ColorizeFont("E_CreateFile", 197, 57, 235) + " returned error code!");
                     D_Situate.E_SetCode(1);
-                    D_Situate.E_PushDetail(__StandardLibrary::E_ColorizeFront("@File", 57, 146, 235) + " with name " + __StandardLibrary::E_ColorizeFront(S_FileName, 255, 0, 0) + " already exist!");
+                    D_Situate.E_PushDetail(__StandardLibrary::E_ColorizeFont("@File", 57, 146, 235) + " with name " + __StandardLibrary::E_ColorizeFont(S_FileName, 255, 0, 0) + " already exist!");
                 
                     return D_Situate;
                 }
@@ -98,9 +98,9 @@
                 D_FileController.close();
 
                 D_Situate.E_SetHeader("__Kernel::__Drivers::E_CreateFile()");
-                D_Situate.E_SetBody(__StandardLibrary::E_ColorizeFront("E_CreateFile", 197, 57, 235) + " returned succes code!");
+                D_Situate.E_SetBody(__StandardLibrary::E_ColorizeFont("E_CreateFile", 197, 57, 235) + " returned succes code!");
                 D_Situate.E_SetCode(0);
-                D_Situate.E_PushDetail(__StandardLibrary::E_ColorizeFront("@File", 57, 146, 235) + " with name " + __StandardLibrary::E_ColorizeFront(S_FileName, 255, 0, 0) + " successfully created!");
+                D_Situate.E_PushDetail(__StandardLibrary::E_ColorizeFont("@File", 57, 146, 235) + " with name " + __StandardLibrary::E_ColorizeFont(S_FileName, 255, 0, 0) + " successfully created!");
             
                 return D_Situate;
             }
@@ -110,9 +110,9 @@
 
                 if (E_DirectoryExist(S_DirectoryName).E_GetCode() == 0) {
                     D_Situate.E_SetHeader("__Kernel::__Drivers::E_CreateDirecotry()");
-                    D_Situate.E_SetBody(__StandardLibrary::E_ColorizeFront("E_CreateDirecotry", 197, 57, 235) + " returned error code!");
+                    D_Situate.E_SetBody(__StandardLibrary::E_ColorizeFont("E_CreateDirecotry", 197, 57, 235) + " returned error code!");
                     D_Situate.E_SetCode(1);
-                    D_Situate.E_PushDetail(__StandardLibrary::E_ColorizeFront("@Directory", 57, 146, 235) + " with name " + __StandardLibrary::E_ColorizeFront(S_DirectoryName, 255, 0, 0) + " already exist!");
+                    D_Situate.E_PushDetail(__StandardLibrary::E_ColorizeFont("@Directory", 57, 146, 235) + " with name " + __StandardLibrary::E_ColorizeFont(S_DirectoryName, 255, 0, 0) + " already exist!");
                 
                     return D_Situate;
                 }
@@ -120,9 +120,9 @@
                 _mkdir(S_DirectoryName.c_str());
 
                 D_Situate.E_SetHeader("__Kernel::__Drivers::E_CreateDirecotry()");
-                D_Situate.E_SetBody(__StandardLibrary::E_ColorizeFront("E_CreateDirecotry", 197, 57, 235) + " returned succes code!");
+                D_Situate.E_SetBody(__StandardLibrary::E_ColorizeFont("E_CreateDirecotry", 197, 57, 235) + " returned succes code!");
                 D_Situate.E_SetCode(0);
-                D_Situate.E_PushDetail(__StandardLibrary::E_ColorizeFront("@Directory", 57, 146, 235) + " with name " + __StandardLibrary::E_ColorizeFront(S_DirectoryName, 255, 0, 0) + " successfully created!");
+                D_Situate.E_PushDetail(__StandardLibrary::E_ColorizeFont("@Directory", 57, 146, 235) + " with name " + __StandardLibrary::E_ColorizeFont(S_DirectoryName, 255, 0, 0) + " successfully created!");
             
                 return D_Situate;
             }
@@ -132,9 +132,9 @@
 
                 if (E_FileExist(S_FileName).E_GetCode() == 0) {
                     D_Situate.E_SetHeader("__Kernel::__Drivers::E_RemoveFile()");
-                    D_Situate.E_SetBody(__StandardLibrary::E_ColorizeFront("E_RemoveFile", 197, 57, 235) + " returned error code!");
+                    D_Situate.E_SetBody(__StandardLibrary::E_ColorizeFont("E_RemoveFile", 197, 57, 235) + " returned error code!");
                     D_Situate.E_SetCode(1);
-                    D_Situate.E_PushDetail(__StandardLibrary::E_ColorizeFront("@File", 57, 146, 235) + " with name " + __StandardLibrary::E_ColorizeFront(S_FileName, 255, 0, 0) + " doesn't exist!");
+                    D_Situate.E_PushDetail(__StandardLibrary::E_ColorizeFont("@File", 57, 146, 235) + " with name " + __StandardLibrary::E_ColorizeFont(S_FileName, 255, 0, 0) + " doesn't exist!");
                 
                     return D_Situate;
                 }
@@ -142,9 +142,9 @@
                 std::filesystem::remove(S_FileName);
 
                 D_Situate.E_SetHeader("__Kernel::__Drivers::E_RemoveFile()");
-                D_Situate.E_SetBody(__StandardLibrary::E_ColorizeFront("E_RemoveFile", 197, 57, 235) + " returned succes code!");
+                D_Situate.E_SetBody(__StandardLibrary::E_ColorizeFont("E_RemoveFile", 197, 57, 235) + " returned succes code!");
                 D_Situate.E_SetCode(0);
-                D_Situate.E_PushDetail(__StandardLibrary::E_ColorizeFront("@File", 57, 146, 235) + " with name " + __StandardLibrary::E_ColorizeFront(S_FileName, 255, 0, 0) + " successfully removed!");
+                D_Situate.E_PushDetail(__StandardLibrary::E_ColorizeFont("@File", 57, 146, 235) + " with name " + __StandardLibrary::E_ColorizeFont(S_FileName, 255, 0, 0) + " successfully removed!");
             
                 return D_Situate;
             }
@@ -154,9 +154,9 @@
 
                 if (E_DirectoryExist(S_DirectoryName).E_GetCode() == 0) {
                     D_Situate.E_SetHeader("__Kernel::__Drivers::E_RemoveDirectory()");
-                    D_Situate.E_SetBody(__StandardLibrary::E_ColorizeFront("E_RemoveDirectory", 197, 57, 235) + " returned error code!");
+                    D_Situate.E_SetBody(__StandardLibrary::E_ColorizeFont("E_RemoveDirectory", 197, 57, 235) + " returned error code!");
                     D_Situate.E_SetCode(1);
-                    D_Situate.E_PushDetail(__StandardLibrary::E_ColorizeFront("@Directory", 57, 146, 235) + " with name " + __StandardLibrary::E_ColorizeFront(S_DirectoryName, 255, 0, 0) + " doesn't exist!");
+                    D_Situate.E_PushDetail(__StandardLibrary::E_ColorizeFont("@Directory", 57, 146, 235) + " with name " + __StandardLibrary::E_ColorizeFont(S_DirectoryName, 255, 0, 0) + " doesn't exist!");
                 
                     return D_Situate;
                 }
@@ -164,9 +164,9 @@
                 std::filesystem::remove_all(S_DirectoryName);
 
                 D_Situate.E_SetHeader("__Kernel::__Drivers::E_RemoveDirectory()");
-                D_Situate.E_SetBody(__StandardLibrary::E_ColorizeFront("E_RemoveDirectory", 197, 57, 235) + " returned succes code!");
+                D_Situate.E_SetBody(__StandardLibrary::E_ColorizeFont("E_RemoveDirectory", 197, 57, 235) + " returned succes code!");
                 D_Situate.E_SetCode(0);
-                D_Situate.E_PushDetail(__StandardLibrary::E_ColorizeFront("@Directory", 57, 146, 235) + " with name " + __StandardLibrary::E_ColorizeFront(S_DirectoryName, 255, 0, 0) + " successfully removed!");
+                D_Situate.E_PushDetail(__StandardLibrary::E_ColorizeFont("@Directory", 57, 146, 235) + " with name " + __StandardLibrary::E_ColorizeFont(S_DirectoryName, 255, 0, 0) + " successfully removed!");
             
                 return D_Situate;
             }
@@ -176,9 +176,9 @@
 
                 if (E_FileExist(S_FileName).E_GetCode() != 0) {
                     D_Situate.E_SetHeader("__Kernel::__Drivers::E_ReadFile()");
-                    D_Situate.E_SetBody(__StandardLibrary::E_ColorizeFront("E_ReadFile", 197, 57, 235) + " returned error code!");
+                    D_Situate.E_SetBody(__StandardLibrary::E_ColorizeFont("E_ReadFile", 197, 57, 235) + " returned error code!");
                     D_Situate.E_SetCode(1);
-                    D_Situate.E_PushDetail(__StandardLibrary::E_ColorizeFront("@File", 57, 146, 235) + " with name " + __StandardLibrary::E_ColorizeFront(S_FileName, 255, 0, 0) + " doesn't exist!");
+                    D_Situate.E_PushDetail(__StandardLibrary::E_ColorizeFont("@File", 57, 146, 235) + " with name " + __StandardLibrary::E_ColorizeFont(S_FileName, 255, 0, 0) + " doesn't exist!");
                 
                     return D_Situate;
                 }
@@ -191,7 +191,7 @@
                 }
 
                 D_Situate.E_SetHeader("__Kernel::__Drivers::E_ReadFile()");
-                D_Situate.E_SetBody(__StandardLibrary::E_ColorizeFront("E_ReadFile", 197, 57, 235) + " returned success code!");
+                D_Situate.E_SetBody(__StandardLibrary::E_ColorizeFont("E_ReadFile", 197, 57, 235) + " returned success code!");
                 D_Situate.E_SetCode(0);
                 
                 for (std::string D_Line : D_ParsedFile) {
@@ -218,7 +218,7 @@
                 D_FileController.close();
 
                 D_Situate.E_SetHeader("__Kernel::__Drivers::E_Rewrite()");
-                D_Situate.E_SetBody(__StandardLibrary::E_ColorizeFront("E_ReadFile", 197, 57, 235) + " returned success code!");
+                D_Situate.E_SetBody(__StandardLibrary::E_ColorizeFont("E_ReadFile", 197, 57, 235) + " returned success code!");
                 D_Situate.E_SetCode(0);
 
                 return D_Situate;
@@ -232,10 +232,10 @@
                     std::string D_Name = __StandardLibrary::E_Split(entry.path().string(), "/").at(__StandardLibrary::E_Split(entry.path().string(), "/").size() - 1);
 
                     if (entry.is_directory()) {
-                        D_Situate.E_PushDetail(__StandardLibrary::E_ColorizeBack(__StandardLibrary::E_ColorizeFront(D_Name, 0, 0, 0), 235, 235, 52));
+                        D_Situate.E_PushDetail(__StandardLibrary::E_ColorizeBackground(__StandardLibrary::E_ColorizeFont(D_Name, 0, 0, 0), 235, 235, 52));
                     }
                     else {
-                        D_Situate.E_PushDetail(__StandardLibrary::E_ColorizeBack(__StandardLibrary::E_ColorizeFront(D_Name, 0, 0, 0), 104, 235, 52));
+                        D_Situate.E_PushDetail(__StandardLibrary::E_ColorizeBackground(__StandardLibrary::E_ColorizeFont(D_Name, 0, 0, 0), 104, 235, 52));
                     }
                 }
 
