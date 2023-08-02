57388d97f71511adea9aac3902c0175b386616ed
ruletkasuperstar <maksim.manezkiy.evg.99@gmail.com>
2023-08-02 12:03:16 +0300

    0.0.0
    Создана полнофункциональная система взаимодействия с оболочкой

--- misc/TTY.h
+++ misc/TTY.h
@@ -5,6 +5,8 @@
 #include <vector>
 #include <string>
 #include <functional>
+#include "./Utilities.h"
+#include "./Screen.h"
 
 namespace misc
 {
@@ -19,9 +21,58 @@ namespace misc
     private:
         std::vector<Command> _commands;
     public:
-        void CreateCommand()
+        void CreateCommand(std::string commnad, std::function<void(std::vector<std::string>)> function)
         {
-            
+            int checker = this->CheckIfExists(commnad);
+
+            if (checker != -1)
+            {
+                this->_commands.at(checker).executor = function;
+            }
+            else
+            {
+                Command newCommand = { commnad, function };
+
+                this->_commands.push_back(newCommand);
+            }
+        }
+
+        void StartInteractive()
+        {
+            while(true)
+            {
+                std::string UserInput;
+
+                misc::Input(UserInput);
+
+                int checker = this->CheckIfExists(UserInput);
+
+                if (checker == -1)
+                {
+                    misc::Output("Command not found\n");
+                }
+                else
+                {
+                    Command command = this->_commands.at(checker);
+
+                    command.executor(std::vector<std::string>{ "1" });
+                }
+            }
+        }
+
+        int CheckIfExists(std::string command)
+        {
+            for (int i = 0; i < this->_commands.size(); i++)
+            {
+                Command existsCommand = this->_commands.at(i);
+
+                if (existsCommand.command.compare(command) == 0)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
         }
     };
 }
