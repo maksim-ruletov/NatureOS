5c45eeadc08e750e4c09f9a0d85dcc0166ae2942
ruletkasuperstar <maksim.manezkiy.evg.99@gmail.com>
2023-08-02 17:57:17 +0300

    0.0.0

--- arch/shell.h
+++ arch/shell.h
@@ -9,6 +9,8 @@
 #include "../misc/Colorizer.h"
 #include "../misc/TTY.h"
 
+#include "./sys.h"
+
 namespace arch {
     class Shell
     {
@@ -19,11 +21,31 @@ namespace arch {
 
             misc::Clear();
 
-            misc::Output(misc::E_ColorizeFont("Welcome to NatureOS shell!\n", 255, 0, 0));
+            misc::Output(misc::ColorizeFont("Welcome to NatureOS shell!\n", 255, 0, 0));
 
-            tty.CreateCommand("test", [](std::vector<std::string> arguments)
+            tty.CreateCommand("help", [](std::vector<std::string> arguments)
             {
-                misc::Output("test\n");
+                misc::Output(misc::ColorizeFont("NatureShell help list", 0, 255, 255));
+                misc::Output("\n");
+                misc::Output("\n");
+                misc::Output(misc::ColorizeFont("sys", 0, 100, 255) + " - information about system\n");
+            });
+
+            tty.CreateCommand("sys", [](std::vector<std::string> arguments)
+            {
+                if (arguments.size() == 0)
+                {
+                    misc::Output(misc::ColorizeFont("System information", 0, 255, 255));
+                    misc::Output("\n");
+                    misc::Output("\n");
+                    misc::Output("@sys command require arguments. If you want to learn about it write sys -h\n");
+                }
+                else {
+                    if (arguments.at(0).compare("-i") == 0)
+                    {
+                        misc::Output(StringifyInfo());
+                    }
+                }
             });
 
             tty.StartInteractive();
