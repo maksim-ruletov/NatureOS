b8f62856fd62105275f41b5880901daae31c94b7
ruletkasuperstar <maksim.manezkiy.evg.99@gmail.com>
2023-08-02 17:56:29 +0300

    0.0.0
    Добавлена информация о системе

---
+++ arch/sys.h
@@ -0,0 +1,25 @@
+#if !defined(L0_SYS_H)
+
+#define L0_SYS_H
+
+#include <string>
+#include "../misc/Colorizer.h"
+
+namespace arch
+{
+    const std::string SysVersion = "0.0.0";
+    const std::string SysName = "Nature OS (POS)";
+    const std::string SysAuthor = "ruletkasuperstar";
+
+    std::string StringifyInfo()
+    {
+        std::string output = "| " + misc::ColorizeFont(SysVersion, 100, 0, 10) + " | " + misc::ColorizeFont(SysName, 100, 0, 10) + " |\n";
+        output += "Made by " + misc::ColorizeFont(SysAuthor, 255, 0, 0) + "\n\n";
+        output += misc::ColorizeBackground("   ", 255, 0, 0) + misc::ColorizeBackground("   ", 0, 255, 0) + misc::ColorizeBackground("   ", 0, 0, 255)
+                  + misc::ColorizeBackground("   ", 255, 255, 0) + misc::ColorizeBackground("   ", 0, 255, 255) + misc::ColorizeBackground("   ", 255, 255, 255) + "\n\n";
+
+        return output;
+    }
+}
+
+#endif
