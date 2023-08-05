76ef9b38fb383782c4381519b19e1e026c604c15
ruletkasuperstar <maksim.manezkiy.evg.99@gmail.com>
2023-08-05 09:29:20 +0300

    0.1.0
    Добавлена базовая разметка ОС

--- fs/translated.h
+++ fs/translated.h
@@ -2,3 +2,10 @@
 #include "./translated/checkDirectory.h"
 #include "./translated/createFile.h"
 #include "./translated/createDirectory.h"
+
+#include <vector>
+#include <string>
+
+const std::vector<std::string> RequiredDirectories { "natureos", "natureos/bin", "natureos/system", "natureos/system/config", "natureos/system/cache", "natureos/local" };
+const std::vector<std::string> RequiredUserDirectories { "natureos/home" };
+const std::vector<std::string> OptionalDirectories { "natureos/cache", "natureos/fscache" };
