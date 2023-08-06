20ed52b1fbeef6014610d4be7051efa01350ff5e
ruletkasuperstar <maksim.manezkiy.evg.99@gmail.com>
2023-08-07 00:26:47 +0300

    0.3.0
    Создана система установки файловой системы ОС

---
+++ installer/main.h
@@ -0,0 +1,105 @@
+#if !defined(L1_MAIN_H)
+
+#define L1_MAIN_H
+
+#include <string>
+#include <vector>
+#include <cstdint>
+
+#include "../misc/Screen.h"
+#include "../misc/Colorizer.h"
+
+namespace installer
+{
+    int CreateFileSystem(std::string installPreset)
+    {
+        if (installPreset.compare("f") != 0 && installPreset.compare("c") != 0)
+        {
+            return -1;
+        }
+        else
+        {
+            const std::vector<std::string> Markers {
+                misc::ColorizeBackground("  ", 255, 50, 0), // error
+                misc::ColorizeBackground("  ", 0, 255, 50), // success
+                misc::ColorizeBackground("  ", 255, 255, 0), // warn
+                misc::ColorizeBackground("  ", 0, 0, 255) // info
+            };
+
+            // Full Setup
+            if (installPreset.compare("f") == 0)
+            {
+                for (std::string path : RequiredDirectories)
+                {
+                    if (!fs::translated::DirectoryExist(path))
+                    {
+                        misc::Output(Markers.at(3) + " >> Path " + path + " not found, creating...\n");
+                        fs::translated::CreateDirectory(path);
+                    }
+                    else
+                    {
+                        misc::Output(Markers.at(1) + " >> Path " + path + " founded!\n");
+                    }
+                }
+
+                for (std::string path : RequiredUserDirectories)
+                {
+                    if (!fs::translated::DirectoryExist(path))
+                    {
+                        misc::Output(Markers.at(3) + " >> User path " + path + " not found, creating...\n");
+                        fs::translated::CreateDirectory(path);
+                    }
+                    else
+                    {
+                        misc::Output(Markers.at(1) + " >> User path " + path + " founded!\n");
+                    }
+                }
+
+                for (std::string path : OptionalDirectories)
+                {
+                    if (!fs::translated::DirectoryExist(path))
+                    {
+                        misc::Output(Markers.at(3) + " >> Path " + path + " not found, creating...\n");
+                        fs::translated::CreateDirectory(path);
+                    }
+                    else
+                    {
+                        misc::Output(Markers.at(1) + " >> User path " + path + " founded!\n");
+                    }
+                }
+            }
+            else
+            {
+                for (std::string path : RequiredDirectories)
+                {
+                    if (!fs::translated::DirectoryExist(path))
+                    {
+                        misc::Output(Markers.at(3) + " >> Path " + path + " not found, creating...\n");
+                        fs::translated::CreateDirectory(path);
+                    }
+                    else
+                    {
+                        misc::Output(Markers.at(1) + " >> Path " + path + " founded!\n");
+                    }
+                }
+
+                for (std::string path : RequiredUserDirectories)
+                {
+                    if (!fs::translated::DirectoryExist(path))
+                    {
+                        misc::Output(Markers.at(3) + " >> User path " + path + " not found, creating...\n");
+                        fs::translated::CreateDirectory(path);
+                    }
+                    else
+                    {
+                        misc::Output(Markers.at(1) + " >> User path " + path + " founded!\n");
+                    }
+                }
+            }
+
+            return 0;
+        }
+    }
+} // #installer
+
+#endif
